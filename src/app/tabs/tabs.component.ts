0ab35fd2cb578c8729454e25aee5e6891fd44557
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-25 11:33:30 +0200

    bookmarks link

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })

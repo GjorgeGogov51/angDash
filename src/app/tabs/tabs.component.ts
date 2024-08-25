31a97a2a5c2b9a7dbcb79381bf6176ad456305ea
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-25 13:05:44 +0200

    todos and notes + selected

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })

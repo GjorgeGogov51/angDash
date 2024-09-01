ba013a592e8d8938caa3a7a74340c148a28aad10
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-09-01 12:40:39 +0200

    animations setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,TabsComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations:[
+    trigger('routeAnim',[
+      transition('* => *',[
+        style({
+          backgroundColor: 'blue'
+        }),
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-personal-dashboard';
+  prepareRoute(outlet:RouterOutlet){
+    if(outlet.isActivated) 
+      return outlet.activatedRoute.snapshot.url;
+
+    return;
+  }
 }

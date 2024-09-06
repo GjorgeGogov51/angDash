937646672e3ce439a12607f81bbe365de0a2553a
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-09-06 17:59:43 +0200

    bidirectional animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ import { animate, group, query, state, style, transition, trigger } from '@angul
   styleUrl: './app.component.scss',
   animations:[
     trigger('routeAnim',[
-      transition('* => *',[
+      // * => *
+      transition(':increment',[
         //Parent element (can also be in SCSS file)
         style({
           position: 'relative',
@@ -40,23 +41,61 @@ import { animate, group, query, state, style, transition, trigger } from '@angul
             animate('200ms ease-in',
               style({
                 opacity: '0',
-                transform: 'translateX(-80px)',
+                transform: 'translateX(-50px)',
               }))
           ],{optional:true}),
   
           query(':enter',[
             style({
-              transform: 'translateX(80px)',
+              transform: 'translateX(50px)',
               opacity:'0'
             }),
-            animate(200,
+            animate('250ms 120ms ease-out', //120ms delay when entering
               style({
                 opacity: '1',
                 transform: 'translateX(0px)'
               }))
           ],{optional:true}),
         ]),
+      ]),
 
+      transition(':decrement',[
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(':enter, :leave',[
+          style({
+            position:'absolute',
+            top:0,
+            left:0,
+            width:'100%',
+            height:'100%'
+          })
+        ], {optional: true}),
+
+        group([
+          query(':leave',[
+            animate('200ms ease-in',
+              style({
+                opacity: '0',
+                transform: 'translateX(50px)',
+              }))
+          ],{optional:true}),
+  
+          query(':enter',[
+            style({
+              transform: 'translateX(-50px)',
+              opacity:'0'
+            }),
+            animate('250ms 120ms ease-out',
+              style({
+                opacity: '1',
+                transform: 'translateX(0px)'
+              }))
+          ],{optional:true}),
+        ]),
       ])
     ])
   ]
@@ -64,7 +103,8 @@ import { animate, group, query, state, style, transition, trigger } from '@angul
 export class AppComponent {
   prepareRoute(outlet:RouterOutlet){
     if(outlet.isActivated) 
-      return outlet.activatedRoute.snapshot.url;
+      return outlet.activatedRouteData['tabNumber']
+      //return outlet.activatedRoute.snapshot.url;
 
     return;
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { TodosComponent } from './todos/todos.component';
 import { NotesComponent } from './notes/notes.component';
 
 export const routes: Routes = [
-    { path: 'bookmarks', component: BookmarksComponent},
-    { path: 'todos', component: TodosComponent},
-    { path: 'notes', component: NotesComponent}
+    { path: 'bookmarks', component: BookmarksComponent, data: { tabNumber: 1}},
+    { path: 'todos', component: TodosComponent, data: { tabNumber: 2}},
+    { path: 'notes', component: NotesComponent, data: { tabNumber: 3}}
 ];
